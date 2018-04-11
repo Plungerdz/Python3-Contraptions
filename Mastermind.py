ab6e41b09e7cb1be6960ebe18a29cd4b444d0791
Plungerdz <rusanrazvan@gmail.com>
2018-04-11 17:55:21 +0300

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,40 +1,55 @@
 from random import choice as ch
-colors = ["R", "V", "P", "A", "G","M"]
+from random import shuffle as sf
+
+colors = ["R", "V", "P", "A", "G", "M"]
 code = ""
 for i in range(4):
-	code+=ch(colors)
-code="RRPM"
+    code += ch(colors)
 
-ok=False
-
-def fb(guess):
-	l=[i for i in code]
-	print("l: ",l)
-	for i in range(len(guess)):
-		part="_"
-		if guess[i] in l:
-			part="N"
-		if guess[i]==code[i]:
-			part="A"
-		l[i]="*"
-		yield part
+ok = False
 
 def toast(guess):
-	s=""
-	for i in guess:
-		s+=i
-	return s
-		
+    s = ""
+    for i in guess:
+        s += i
+    return s
+
+def fbi(guess):
+    l = [i for i in code]
+    aux=[i for i in guess]
+    for i in range(len(l)):
+        part="_"
+        if aux[i]==l[i]:
+            part="A"
+            l[i]="*"
+            aux[i]="*"
+        yield part
+    yield l
+    yield guess
+
+def fbs(fbi):
+    fbi=list(fbi)
+    ln=len(fbi)
+    t=fbi[:ln-2]
+    l=fbi[ln-2]
+    guess=fbi[ln-1]
+    for i in range(len(l)):
+        if (guess[i] in l) and (t[i]!="A"):
+            l[i]="*"
+            t[i]="N"
+    return t
+
 for i in range(12):
-	guess=input("your guess here:")
-	output=toast(list(fb(guess)))
-	print(guess, " ", output)
-	if output=="AAAA":
-		ok=True
-		break
+    guess = input("your guess here:")
+    output = fbs(fbi(guess))
+    sf(output)
+    output=toast(output)
+    print(guess, " ", output)
+    if output == "AAAA":
+        ok = True
+        break
 
 if ok:
-	print("You've won!")
+    print("You've won!")
 else:
-	print("You've lost!")
-	
+    print("You've lost!")
